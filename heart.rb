b84c8ffbee6b9dc4786b6f3093e84ab47af1960d
SHIRIN A.S <83756732+shirinas2002@users.noreply.github.com>
2022-02-12 21:47:28 +0530

    Add files via upload

---
+++ heart.rb
@@ -0,0 +1,8 @@
+puts "     lovelo   velove"
+puts "   lovelovelovelovelove"
+puts " lovelovelovelovelovelov"
+puts "  lovelovelovelovelovelo"
+puts "    lovelovelovelovelo"
+puts "      lolovelovelo"
+puts "        lovelove"
+puts "           ve"
